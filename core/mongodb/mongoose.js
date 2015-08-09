53b5e34f15bd8ee13ad8cf6de190e3883bd3a603
Khalid Hoffman <kahdev15@gmail.com>
2015-08-09 18:24:20 -0400

    - Major Update (Alpha)

---
+++ core/mongodb/mongoose.js
@@ -0,0 +1,44 @@
+var Task = null,
+    User = null;
+
+/**
+ *
+ * @param isDev
+ */
+module.exports.connect = function(isDev){
+    var mongoose = require('mongoose'),
+        isDevelopment = isDev || require('os').hostname().toLowerCase().indexOf('kah')>-1;
+
+    var username = '***REMOVED***',
+        password = '***REMOVED***';
+
+    mongoose.connect('mongodb://'+username+':'+password+'@***REMOVED***');
+
+    var db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error:'));
+
+    db.once('open', function (callback) {
+        console.log('successfully connected.');
+    });
+
+    var TaskSchema = new mongoose.Schema( require('./schemas/task'));
+    var UserSchema = new mongoose.Schema( require('./schemas/user'));
+
+    if (isDevelopment){
+        console.log('Running in dev mode.');
+        Task = mongoose.model('Task', TaskSchema, 'Deadlines_Development');
+        User = mongoose.model('User', UserSchema, 'Deadlines_Users_Development');
+    } else{
+        Task = mongoose.model('Task', TaskSchema, 'Deadlines');
+        User = mongoose.model('User', UserSchema, 'Deadlines_Users');
+    };
+
+    return {
+        schemas : {
+            Task : Task,
+            User : User
+        }
+    }
+
+};
